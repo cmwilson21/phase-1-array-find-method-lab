48583363e29ea365f79e9aa8504dfab7ea08975a
Courtney <cmwilson132@gmail.com>
2021-06-13 16:18:08 -0500

    commit tree eaa515677ad5fcc23b17bf7c24bd2ce571716998

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(records) {
+    const result = records.find(record =>record.result === "W")
+      console.log(result);
+      if (result) {
+          return result.year;
+      }
+ }
+ 
